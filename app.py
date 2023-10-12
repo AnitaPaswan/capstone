63951a3e672901d6da806e2aefa44d35e09c96d2
AnitaPaswan <raiv19892011@gmail.com>
2023-10-13 00:21:54 +0530

    added

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ moment = Moment(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 setup_db(app)
-cors = CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*", "headers": "Authorization", "methods": ["GET", "POST"]}})
 
 @app.after_request
 def after_request(response):
