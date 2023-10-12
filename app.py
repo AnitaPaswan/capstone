12aa462d03ce6b01d7fcfe3523afa1e80b6ecb5e
AnitaPaswan <raiv19892011@gmail.com>
2023-10-13 01:47:11 +0530

    added cors allow

--- app.py
+++ app.py
@@ -25,13 +25,14 @@ moment = Moment(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 setup_db(app)
-cors = CORS(app, resources={r"/api/*": {"origins": "*", "headers": "Authorization", "methods": ["GET", "POST"]}})
+CORS(app, resources={r"/api/*": {"origins": "*", "headers": "Authorization", "methods": ["GET", "POST"]}})
 
 @app.after_request
 def after_request(response):
     response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
     response.headers.add("Access-Control-Allow-Methods", "GET, DELETE, POST, PATCH")
     response.headers.add("Access-Control-Allow-Credentials", "true")  # Allow credentials
+    response.headers.add("Access-Control-Allow-Credentials", "true")
     return response
 
 @app.route('/')
