18d0c8eb67e523d3c99ff0e5a636760ccc64b4de
AnitaPaswan <raiv19892011@gmail.com>
2023-10-14 03:06:15 +0530

    28

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ oauth.register(
 )
 ###############################
 @app.route("/login")
-def login():
+def index():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
     )
